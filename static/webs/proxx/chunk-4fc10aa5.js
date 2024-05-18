2e66a51b1fa256ff8e996679b6da0aa11799eb96
ZSCGR <932478692@qq.com>
2024-05-19 01:33:58 +0800

    fix

---
+++ static/webs/proxx/chunk-4fc10aa5.js
@@ -0,0 +1,2 @@
+define("./chunk-4fc10aa5.js",["exports"],function(e){"use strict";e.bind=function(e,n,t){return{get:function(){var e=t.value.bind(this);return Object.defineProperty(this,n,{value:e}),e}}}});
+//# sourceMappingURL=chunk-4fc10aa5.js.map
