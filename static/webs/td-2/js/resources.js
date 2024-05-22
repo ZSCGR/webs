ca1336d02d8e99d3c824fc359735f23e845dfa40
ZSCGR <932478692@qq.com>
2024-05-23 00:14:55 +0800

    add

---
+++ static/webs/td-2/js/resources.js
@@ -0,0 +1,457 @@
+game.resources = [
+    {
+        "name": "instruction_screen",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "level01bgm",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "lost_island",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "low_point",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "title_music",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "arrow_shoot",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "boss_death",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "cannon_shoot",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "footman_death",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "goblin_death",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "imp_death",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "sell_item",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "slow_shoot",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "tower_drop",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "upgrade",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "footmanchilled",
+        "type": "image",
+        "src": "data/img/enemies/footmanchilled.png"
+    },
+    {
+        "name": "goblinchilled",
+        "type": "image",
+        "src": "data/img/enemies/goblinchilled.png"
+    },
+    {
+        "name": "impchilled",
+        "type": "image",
+        "src": "data/img/enemies/impchilled.png"
+    },
+    {
+        "name": "gameOver",
+        "type": "image",
+        "src": "data/img/gui/gameOver.png"
+    },
+    {
+        "name": "instructions1",
+        "type": "image",
+        "src": "data/img/gui/instructions1.png"
+    },
+    {
+        "name": "instructions2",
+        "type": "image",
+        "src": "data/img/gui/instructions2.png"
+    },
+    {
+        "name": "instructions3",
+        "type": "image",
+        "src": "data/img/gui/instructions3.png"
+    },
+    {
+        "name": "instructions4",
+        "type": "image",
+        "src": "data/img/gui/instructions4.png"
+    },
+    {
+        "name": "instructions5",
+        "type": "image",
+        "src": "data/img/gui/instructions5.png"
+    },
+    {
+        "name": "instructions6",
+        "type": "image",
+        "src": "data/img/gui/instructions6.png"
+    },
+    {
+        "name": "introLevel01",
+        "type": "image",
+        "src": "data/img/gui/introLevel01.png"
+    },
+    {
+        "name": "introLevel02",
+        "type": "image",
+        "src": "data/img/gui/introLevel02.png"
+    },
+    {
+        "name": "introLevel03",
+        "type": "image",
+        "src": "data/img/gui/introLevel03.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "title2",
+        "type": "image",
+        "src": "data/img/gui/title2.png"
+    },
+    {
+        "name": "title3",
+        "type": "image",
+        "src": "data/img/gui/title3.png"
+    },
+    {
+        "name": "title3low",
+        "type": "image",
+        "src": "data/img/gui/title3low.png"
+    },
+    {
+        "name": "winscreen",
+        "type": "image",
+        "src": "data/img/gui/winscreen.png"
+    },
+    {
+        "name": "xxintroLevel01",
+        "type": "image",
+        "src": "data/img/gui/xxintroLevel01.png"
+    },
+    {
+        "name": "xxintroLevel02",
+        "type": "image",
+        "src": "data/img/gui/xxintroLevel02.png"
+    },
+    {
+        "name": "xxintroLevel03",
+        "type": "image",
+        "src": "data/img/gui/xxintroLevel03.png"
+    },
+    {
+        "name": "HUD_bar",
+        "type": "image",
+        "src": "data/img/HUD/HUD_bar.png"
+    },
+    {
+        "name": "level_complete",
+        "type": "image",
+        "src": "data/img/HUD/level_complete.png"
+    },
+    {
+        "name": "level_failed",
+        "type": "image",
+        "src": "data/img/HUD/level_failed.png"
+    },
+    {
+        "name": "pause_menu",
+        "type": "image",
+        "src": "data/img/HUD/pause_menu.png"
+    },
+    {
+        "name": "pause_text",
+        "type": "image",
+        "src": "data/img/HUD/pause_text.png"
+    },
+    {
+        "name": "Castle",
+        "type": "image",
+        "src": "data/img/maps/Castle.png"
+    },
+    {
+        "name": "Castle2",
+        "type": "image",
+        "src": "data/img/maps/Castle2.png"
+    },
+    {
+        "name": "erodaxia summary",
+        "type": "image",
+        "src": "data/img/maps/erodaxia summary.png"
+    },
+    {
+        "name": "map01",
+        "type": "image",
+        "src": "data/img/maps/map01.png"
+    },
+    {
+        "name": "map02",
+        "type": "image",
+        "src": "data/img/maps/map02.png"
+    },
+    {
+        "name": "map03",
+        "type": "image",
+        "src": "data/img/maps/map03.png"
+    },
+    {
+        "name": "stone-bridge",
+        "type": "image",
+        "src": "data/img/maps/stone-bridge.png"
+    },
+    {
+        "name": "tilesheet",
+        "type": "image",
+        "src": "data/img/maps/tilesheet.png"
+    },
+    {
+        "name": "trees_plants_rocks",
+        "type": "image",
+        "src": "data/img/maps/trees_plants_rocks.png"
+    },
+    {
+        "name": "wood_tileset",
+        "type": "image",
+        "src": "data/img/maps/wood_tileset.png"
+    },
+    {
+        "name": "arrow_bleed_icons",
+        "type": "image",
+        "src": "data/img/menu/arrow_bleed_icons.png"
+    },
+    {
+        "name": "arrow_icons",
+        "type": "image",
+        "src": "data/img/menu/arrow_icons.png"
+    },
+    {
+        "name": "arrow_multi_icons",
+        "type": "image",
+        "src": "data/img/menu/arrow_multi_icons.png"
+    },
+    {
+        "name": "cancel_icon",
+        "type": "image",
+        "src": "data/img/menu/cancel_icon.png"
+    },
+    {
+        "name": "cannon_aoe_icons",
+        "type": "image",
+        "src": "data/img/menu/cannon_aoe_icons.png"
+    },
+    {
+        "name": "cannon_damage_icons",
+        "type": "image",
+        "src": "data/img/menu/cannon_damage_icons.png"
+    },
+    {
+        "name": "cannon_icons",
+        "type": "image",
+        "src": "data/img/menu/cannon_icons.png"
+    },
+    {
+        "name": "icons",
+        "type": "image",
+        "src": "data/img/menu/icons.png"
+    },
+    {
+        "name": "menu_button",
+        "type": "image",
+        "src": "data/img/menu/menu_button.png"
+    },
+    {
+        "name": "menu_circle",
+        "type": "image",
+        "src": "data/img/menu/menu_circle.png"
+    },
+    {
+        "name": "option_menu",
+        "type": "image",
+        "src": "data/img/menu/option_menu.png"
+    },
+    {
+        "name": "sell_icon",
+        "type": "image",
+        "src": "data/img/menu/sell_icon.png"
+    },
+    {
+        "name": "sell_icons",
+        "type": "image",
+        "src": "data/img/menu/sell_icons.png"
+    },
+    {
+        "name": "slow_damage_icons",
+        "type": "image",
+        "src": "data/img/menu/slow_damage_icons.png"
+    },
+    {
+        "name": "slow_icons",
+        "type": "image",
+        "src": "data/img/menu/slow_icons.png"
+    },
+    {
+        "name": "slow_slower_icons",
+        "type": "image",
+        "src": "data/img/menu/slow_slower_icons.png"
+    },
+    {
+        "name": "tower_location",
+        "type": "image",
+        "src": "data/img/menu/tower_location.png"
+    },
+    {
+        "name": "upgraded_icon",
+        "type": "image",
+        "src": "data/img/menu/upgraded_icon.png"
+    },
+    {
+        "name": "smallFire",
+        "type": "image",
+        "src": "data/img/other/smallFire.png"
+    },
+    {
+        "name": "arrows",
+        "type": "image",
+        "src": "data/img/projectiles/arrows.png"
+    },
+    {
+        "name": "bleedEffect",
+        "type": "image",
+        "src": "data/img/projectiles/bleedEffect.png"
+    },
+    {
+        "name": "cannonball",
+        "type": "image",
+        "src": "data/img/projectiles/cannonball.png"
+    },
+    {
+        "name": "explosion",
+        "type": "image",
+        "src": "data/img/projectiles/explosion.png"
+    },
+    {
+        "name": "freeze_exp",
+        "type": "image",
+        "src": "data/img/projectiles/freeze_exp.png"
+    },
+    {
+        "name": "archer_tower",
+        "type": "image",
+        "src": "data/img/towers/archer_tower.png"
+    },
+    {
+        "name": "cannon_tower",
+        "type": "image",
+        "src": "data/img/towers/cannon_tower.png"
+    },
+    {
+        "name": "slow_tower",
+        "type": "image",
+        "src": "data/img/towers/slow_tower.png"
+    },
+    {
+        "name": "map01",
+        "type": "tmx",
+        "src": "data/map/map01.tmx"
+    },
+    {
+        "name": "map01backup",
+        "type": "tmx",
+        "src": "data/map/map01backup.tmx"
+    },
+    {
+        "name": "map01Intro",
+        "type": "tmx",
+        "src": "data/map/map01Intro.tmx"
+    },
+    {
+        "name": "map02",
+        "type": "tmx",
+        "src": "data/map/map02.tmx"
+    },
+    {
+        "name": "map02backup",
+        "type": "tmx",
+        "src": "data/map/map02backup.tmx"
+    },
+    {
+        "name": "map02Intro",
+        "type": "tmx",
+        "src": "data/map/map02Intro.tmx"
+    },
+    {
+        "name": "map03",
+        "type": "tmx",
+        "src": "data/map/map03.tmx"
+    },
+    {
+        "name": "map03backup",
+        "type": "tmx",
+        "src": "data/map/map03backup.tmx"
+    },
+    {
+        "name": "map03Intro",
+        "type": "tmx",
+        "src": "data/map/map03Intro.tmx"
+    },
+    {
+        "name": "font",
+        "type": "binary",
+        "src": "data/fnt/font.fnt"
+    },
+    {
+        "name": "moneyfont",
+        "type": "binary",
+        "src": "data/fnt/moneyfont.fnt"
+    },
+    {
+        "name": "font",
+        "type": "image",
+        "src": "data/fnt/font.png"
+    },
+    {
+        "name": "moneyfont",
+        "type": "image",
+        "src": "data/fnt/moneyfont.png"
+    }
+];
